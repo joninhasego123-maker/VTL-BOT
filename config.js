811d0051ad83a7e1fbe18294f9d3866de8e82120
joninhasego123-maker <joninhasego123@gmail.com>
2026-09-24 09:05:17 -0300

    Add configuration settings in config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    TOKEN: process.env.TOKEN,
+
+    // =========================
+    // TICKETS
+    // =========================
+
+    TICKET_CATEGORY_ID: "1552649476370604214",
+    STAFF_ROLE_ID: "1552650098637541448",
+    TICKET_PANEL_CHANNEL_ID: "1552649548420087909",
+
+    // =========================
+    // CONTRACT
+    // =========================
+
+    CONTRACT_CHANNEL_ID: "1552636518886019072",
+    CONTRACT_LOG_CHANNEL_ID: "1552634532262584391",
+
+    // =========================
+    // FREE AGENCY
+    // =========================
+
+    FREEAGENCY_CHANNEL_ID: "1552633266446667947"
+};
